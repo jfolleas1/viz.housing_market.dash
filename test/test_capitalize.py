e63560e512f8eef3280089cd6bd7209156911923
Jacques Folleas <jacques.folleas@gmail.com>
2022-10-30 14:55:11 +0100

    add failing test on purpose

--- test/test_capitalize.py
+++ test/test_capitalize.py
@@ -1,8 +1,8 @@
 import pytest
 
 def capital_case(x):
-    if not isinstance(x, str):
-        raise TypeError('Please provide a string argument')
+    # if not isinstance(x, str):
+        # raise TypeError('Please provide a string argument')
     return x.capitalize()
 
 def test_capital_case():
