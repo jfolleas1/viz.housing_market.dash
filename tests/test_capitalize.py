cda16fd585393532f60fbcd699b95b0b02b3690d
Jacques Folleas <jacques.folleas@gmail.com>
2022-10-30 18:33:43 +0100

    remove one of the tests which are for now only here for examles

---
+++ tests/test_capitalize.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+def capital_case(x):
+    if not isinstance(x, str):
+        raise TypeError('Please provide a string argument')
+    return x.capitalize()
+
+
+def test_capital_case():
+    assert capital_case('semaphore') == 'Semaphore'
